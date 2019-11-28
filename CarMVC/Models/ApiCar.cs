92fe00be68971340a7a07c6b3d386ef936418de1
Laurent Voisard <laurentvoisard1@gmail.com>
2019-11-28 15:26:38 -0500

    Added the Models to the Project

---
+++ CarMVC/Models/ApiCar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApiClasses
+{
+    public class ApiCar
+    {
+        public int CarId { get; set; }
+        public string CarColor { get; set; }
+        public string CarType { get; set; }
+        public string CarModel { get; set; }
+        public double CarPrice { get; set; }
+        public double CarCommission { get; set; }
+
+    }
+}
