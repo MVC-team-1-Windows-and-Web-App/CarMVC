e3a939d83b324276796716eb240af38adf3dad64
Kristian Kurktchiev <kristiankurk@gmail.com>
2019-12-16 20:40:22 -0500

    Added location controller, updated the webservice client with appropriate methods, and scaffolded the views.

---
+++ CarMVC/Controllers/LocationController.cs
@@ -0,0 +1,92 @@
+﻿using CarMVCClasses;
+using CarMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarMVC.Controllers
+{
+    public class LocationController : Controller
+    {
+        WebServiceClient client = new WebServiceClient();
+
+        public ActionResult Locations()
+        {
+
+            List<ApiLocation> locations = client.GetLocations();
+
+            return View(locations);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            ApiLocation location = new ApiLocation();
+            location.LocationAddress = form["LocationAddress"];
+            location.LocationStateProv = form["LocationStateProv"];
+            if (!client.CreateLocation(location))
+            {
+                return View(location);
+            }
+            else
+            {
+                return RedirectToAction("Customers");
+            }
+
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            ApiLocation location = client.GetLocation(id);
+
+            return View(location);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, bool? del)
+        {
+
+            if (client.DeleteCustomer(id))
+            {
+                ViewBag.message = "Location ANNIHALATED";
+            }
+            else
+            {
+                ViewBag.message = "Could Not ANNIHALATE location";
+            }
+            return RedirectToAction("Customers");
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(client.GetLocation(id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(client.GetLocation(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ApiLocation location)
+        {
+            if (client.UpdateLocation(location))
+            {
+                ViewBag.message = "Location Changed";
+            }
+            else
+            {
+                ViewBag.message = "Location not Changed, error";
+            }
+            return RedirectToAction("Details", new { id = location.LocationId });
+        }
+    }
+}
